acd238f7847df0dd3b79dd2a53d067a1cf0954aa
John Wong <jwwwwong@gmail.com>
2020-09-30 11:32:27 +0800

    Update config

--- examples/watchDepositAddresses.js
+++ examples/watchDepositAddresses.js
@@ -1,18 +1,16 @@
-// const SDK = require('switcheo-chain-js-sdk') // use this instead if running this sdk as a library
-const SDK = require('../.')
-const { wallet } = SDK
-const { Wallet } = wallet
+const { WalletClient } = require('../build/main')
 
 const net = 'LOCALHOST'
 // const net = 'DEVNET'
 
 async function run() {
-  const mnemonic = 'rely final pipe disease fetch make noodle patch sting hand man digital'
-  const account = await Wallet.connect(mnemonic, net)
-  const address = await account.getDepositAddress('eth')
-  // account.sendNeoDeposits('AK5JtqW3NbdmuxNDjHPo7KuShjjXKz6u6U')
+  const mnemonic = 'joke water monkey survey tip silly satisfy speak document chief elegant hand'
+  const account = await WalletClient.connectMnemonic(mnemonic, net)
+  console.log('connected account')
+  const address = await account.getDepositAddress('eth', net)
   console.log('swth address', account.pubKeyBech32)
-  console.log('address', address)
+  console.log('deposit address', address)
+  await account.watchEthDepositAddress()
 }
 
 run()
